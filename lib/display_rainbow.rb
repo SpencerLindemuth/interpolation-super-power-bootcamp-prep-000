dc1f60c1ab3c6d1c85ce759aece72ba3ba87391c
Spencer Lindemuth <lindemut@gmail.com>
2019-03-06 19:47:41 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(color)
-
+  puts "R: #{color[0]}, Y: #{color[1]}, G: #{color[2]}, B: #{color[3]}, I: #{color[4]}, V: #{color[5]}"
 end
 
 
